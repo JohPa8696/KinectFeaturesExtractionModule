290aa7f0bb2364ed33a8a62032dfa79beb90c6e1
John Pham <dpha010@aucklanduni.ac.nz>
2017-09-06 20:54:27 +1200

    unimportant

--- FallDetectionSystemDataProcessor/FeatureExtractor5.cs
+++ FallDetectionSystemDataProcessor/FeatureExtractor5.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace FallDetectionSystemDataProcessor
 {
-    class FeatureExtractor5
+    class FeatureExtractor5 : IRawDataExtractor
     {
+
+        public string getColumns(int windowSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getID()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] process(List<double[]> data)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
